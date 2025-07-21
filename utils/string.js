062a3ebbe8b2b528512998de02d28aa9e8a6b70f
quapham <qua.pham@amitgroup.vn>
2025-07-21 21:56:17 +0700

    ticket-2: handle empty in capitalize & add toSnakeCase

--- utils/string.js
+++ utils/string.js
@@ -1,5 +1,14 @@
 function capitalize(str) {
+  if (!str) return '';
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function toSnakeCase(str) {
+  return str
+    .replace(/\s+/g, '_')
+    .replace(/[A-Z]/g, (match) => `_${match.toLowerCase()}`)
+    .replace(/^_/, '')
+    .toLowerCase();
 }
 
 module.exports = { capitalize };
